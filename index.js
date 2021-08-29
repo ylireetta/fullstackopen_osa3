221fffa2a384acff0488fb8639bec5bdfc10f6a8
Reetta omalta koneelta <reetta.ylitalo@gmail.com>
2021-08-29 13:11:10 +0300

    Tehtävät 3.4-3.6: Resurssin poistaminen ja lisääminen, virheenkäsittely

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const express = require('express')
 const app = express()
 
+// Otetaan jsonparser käyttöön POSTia varten
+app.use(express.json())
+
+const generateNewID = () => {
+    // Muodostetaan persons-taulukon id-kentistä uusi taulukko
+    // ... on spread-syntaksi, jolla saadaan taulukosta yksittäiset luvut
+    // Haetaan suurin luku tai jos taulukko on tyhjä, otetaan perustaksi nolla
+    
+    const randomValue = () => {
+        return Math.floor(Math.random() * 9999999)
+    }
+
+    let returnValue = 0
+    const personIDs = [...persons].map(n => n.id)
+
+    console.log(personIDs)
+
+    for (let i = 0; i < personIDs.length; i++) {
+        returnValue = randomValue()
+
+        if (!personIDs.some(id => id === returnValue))
+            return returnValue
+    }
+        
+    return -1
+}
+
 let persons = [
     {
         id: 1,
@@ -45,6 +72,39 @@ app.get('/api/persons/:id', (req, res) => {
         res.json(person)
     else
         res.status(404).end()
+})
+
+// Resurssin poistaminen
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
+// Resurssin lisääminen
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (!body.name || !body.number) {
+        return res.status(400).json({
+            error: 'data missing'
+        })
+    }
+
+    if (persons.some(person => person.name == body.name)) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: generateNewID(),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person)
+    res.json(person)
 })
 
 app.get('/info', (req, res) => {
